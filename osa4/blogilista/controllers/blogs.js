30212d835eb9be2b14521185c3fac267246e1cac
Roope Pajunen <roope.pajunen@gmail.com>
2019-05-29 19:27:35 +0300

    Upload osa4

---
+++ osa4/blogilista/controllers/blogs.js
@@ -0,0 +1,114 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(blogs.map(blog => blog.toJSON()))
+})
+
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    console.log('id', request.params.id)
+    const blog = await Blog.findById(request.params.id)
+    console.log('blog', blog)
+    if (blog) {
+      response.json(blog.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  const token = getTokenFrom(request)
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: user._id
+    })
+
+    if (blog.title && blog.url) {
+      try {
+        const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+        response.json(savedBlog.toJSON())
+      } catch (exception) {
+        next(exception)
+      }
+    } else {
+      response.status(400).end()
+    }
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  const token = getTokenFrom(request)
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(request.params.id)
+
+    if (blog.user.toString() === user.id.toString()) {
+      await Blog.findByIdAndRemove(request.params.id)
+      response.status(204).end()
+    }
+
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog.toJSON())
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+module.exports = blogsRouter
---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,259 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const user = new User({ username: 'root', password: 'sekret' })
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length)
+  })
+
+  test('creation fails if password is too short', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'newuser',
+      name: 'Superuser',
+      password: '0',
+    }
+
+    const result = await api
+      .post('/api/users/')
+      .send(newUser)
+      .expect(400)
+
+    expect(result.error.text).toContain('Password must be at least 3')
+
+  })
+
+})
+
+describe('when there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initialBlogs
+      .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(helper.initialBlogs.length)
+  })
+
+  describe('viewing a specific blog', () => {
+
+    test('succeedes with a valid id', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToView = blogsAtStart[0]
+
+      const resultBlog = await api
+        .get(`/api/blogs/${blogToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      expect(resultBlog.body.author).toBe(blogToView.author)
+      expect(resultBlog.body.url).toBe(blogToView.url)
+      expect(resultBlog.body.likes).toBe(blogToView.likes)
+      // this failes expect(resultBlog.body.id).toBe(blogToView.id)
+      // this failes expect(resultBlog.body).toBe(blogToView)
+    })
+
+    test('fails with statuscode 404 if blog does not exist', async () => {
+      const validNonexistingId = await helper.nonExistingId()
+
+      await api
+        .get(`/api/notes/${validNonexistingId}`)
+        .expect(404)
+    })
+
+    test('fails with statuscode 400 if id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a86a3445'
+      // Cast to ObjectId failed for value "5a3d5da59070081a86a3445" at path "_id" for model "Blog"
+      await api
+        .get(`/api/blogs/${invalidId}`)
+        .expect(400)
+    })
+  })
+
+  describe('addition of a new blog', () => {
+
+    test('id field is generated', async () => {
+      const response = await api.get('/api/blogs')
+      expect(response.body[0].id).toBeDefined()
+    })
+
+    test('can be added', async () => {
+      const newBlog = {
+        _id: "5a422bc61b54a671234d17fc",
+        title: "Test title",
+        author: "T.W Tester",
+        url: "http://blog.test.html",
+        likes: 0,
+        __v: 0
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+
+      const contents = blogsAtEnd.map(b => b.title)
+      expect(contents).toContain('Test title')
+    })
+
+    test('like is set to zero', async () => {
+      const newBlog = {
+        _id: "5a422bc61b54a671234d17fc",
+        title: "Test title",
+        author: "T.W Tester",
+        url: "http://blog.test.html",
+        __v: 0
+      }
+
+      const addedBlog = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      expect(addedBlog.body.likes).toBe(0)
+    })
+
+    test('must contain title', async () => {
+      const newBlog = {
+        _id: "5a422bc61b54a683334d17fc",
+        author: "T.W Tester",
+        url: "http://blog.test.html",
+        __v: 0
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    })
+
+    test('must contain url', async () => {
+      const newBlog = {
+        _id: "5a422bc61b54a679898d17fc",
+        title: "Test title",
+        author: "T.W Tester",
+        __v: 0
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    })
+  })
+
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd.length).toBe(
+        helper.initialBlogs.length - 1
+      )
+
+      const contents = blogsAtEnd.map(blog => blog.title)
+
+      expect(contents).not.toContain(blogToDelete.title)
+    })
+  })
+
+  describe('update of a blog', () => {
+
+    test('is successfull', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToUpdate = { ...blogsAtStart[0], likes: 99 }
+
+      const updatedBlog = await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(blogToUpdate)
+        .expect(200)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
+      expect(updatedBlog.body.likes).toBe(blogsAtEnd[0].likes)
+      expect(updatedBlog.body.likes).toBe(99)
+    })
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
